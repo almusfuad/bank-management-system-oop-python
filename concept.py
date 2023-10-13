fee6eadc4d49f5d8953cf7bc55b93ccdc6c9d5df
Almus Fuad <almusfuad@gmail.com>
2023-10-13 16:19:10 +0600

    Analyzing requirement

---
+++ concept.py
@@ -0,0 +1,21 @@
+"""
+    A bank system has 2 types users:
+        1. Clients,
+        2. Admin,
+
+    A bank system gives the permit to Clients:
+        1. Deposit amount
+        2. Withdraw amount
+        3. Check Current Balance
+        4. Check transaction history
+        5. Can take loan
+        6. Transfer amount from ones account to another account
+
+    A bank system gives the permit to Admins:
+        1. Can create account
+        2. Can delete any user account
+        3. Can see all users list
+        4. Check total balance of the bank
+        5. Total available loan amount
+        6. Can on or of loan system
+"""
