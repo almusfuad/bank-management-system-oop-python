6cf8f7b11c2c74880b2d73176b5430c34969d98d
Almus Fuad <almusfuad@gmail.com>
2023-10-13 17:08:54 +0600

    user info gathering

---
+++ user.py
@@ -0,0 +1,11 @@
+class User:
+    def __init__(user, name, email, address, acc_type) -> None:
+        user.name = name
+        user.email = email
+        user.address = address
+        user.acc_type = acc_type
+        user.acc_num = None
+        user.balance = 0
+        user.transaction_history = []
+        user.loan_count = 0
+
