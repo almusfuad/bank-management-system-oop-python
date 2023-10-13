47d69c3691aef791b044d0c00d1d1165e4703ffc
Almus Fuad <almusfuad@gmail.com>
2023-10-13 17:02:07 +0600

    acc create and delete in bank

--- bank.py
+++ bank.py
@@ -1,15 +1,25 @@
 import random
 
 class Bank:
-    def __init__(self, name, address) -> None:
-        self.name = name
-        self.address = address
-        self.init_balance = 1000000
-        self.users = {}
-        self.loan_enabled = True
+    def __init__(bank, name, address) -> None:
+        bank.name = name
+        bank.address = address
+        bank.init_balance = 1000000
+        bank.users = {}
+        bank.loan_enabled = True
 
-    def create_acc(self, name, email, address, acc_type):
+    def create_acc(bank, user):
         account_num = random.randint(10000, 9999)  # Generate a 4-digit account number
-        user = {
-            
-        }
+        user.account_number = account_num
+        user.balance = 0
+        user.transaction_history = []
+        user.load_count = 0
+        bank.users[account_num] = user
+        return account_num
+
+    def delete_acc(bank, account_num):
+        if account_num in bank.users:
+            del bank.users[account_num]
+            print(f'Account {account_num} has been deleted.')
+        else:
+            print('Account does not exist.')
